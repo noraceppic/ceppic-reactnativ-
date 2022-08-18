b0d2c2a016d6900c04bb281df3f3d5da79ca883a
noraceppic <messanora86@gmail.com>
2022-08-18 16:00:14 +0200

    cration input npuveau lien  button ajouter un lien  et aficher list des lien

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import{ useState }   from "react";
+import { StyleSheet, Text, View, TextInput, Button,ScrollView } from "react-native";
 
 export default function App() {
+  const [lienTextSaisie, setLientexteSaisie] = useState('');
+  const [listeliens,setListeLiens]=useState(['www.nora.com']);
+
+  function ajoutLienHandler() {
+   // console.log(lienTextSaisie);
+   setListeLiens([...listeliens,lienTextSaisie]);
+   setLientexteSaisie('');
+
+  }
+
+  function lienInputHandler(textSaisie) {
+    console.log(textSaisie)
+    setLientexteSaisie(textSaisie);
+  }
+
   return (
+   
     <View style={styles.container}>
-      <Text>bienvenu!</Text>
-      <StatusBar style="auto" />
+       <Image source={require('./assets/icon.png')} style={styles.icon} />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="nouveau lien.."
+          style={styles.inputLien}
+          onChangeText={lienInputHandler}
+          value = {lienTextSaisie}
+        />
+        <Button title="Ajouter un lien " onPress={ajoutLienHandler} />
+      </View>
+      <View style={styles.LienContainer}>
+        <Text> Liste des Liens ...</Text>
+        <ScrollView>
+          {listeliens.map((lien)=><Text key={lien}>{lien}</Text>)}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -13,8 +43,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  inputLien: {
+    flex: 1,
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 8,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  LienContainer: {
+  
+    height:80,
+    borderColor:'green',
+    borderWidth :2,
+  },
+  icon:{
+    height:20,
+    width:20,
+  }
 });
